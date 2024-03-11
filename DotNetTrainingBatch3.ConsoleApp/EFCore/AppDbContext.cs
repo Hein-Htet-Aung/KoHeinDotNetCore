38b5d76d5d0bd850110ade31de8449f6f85f100a
Hein Htet <ayathar123@gmail.com>
2024-03-11 21:49:40 +0630

    EF Core Examples

---
+++ DotNetTrainingBatch3.ConsoleApp/EFCore/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using KoHein.ConsoleApp.EFCore.Model;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoHein.ConsoleApp.EFCore
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder OptionBuilder)
+        {
+            SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+            {
+            DataSource = @".\SQL2019E",
+            InitialCatalog = "TestDB",
+            UserID = "sa",
+            Password = "p@ssw0rd",
+            TrustServerCertificate = true
+            };
+            OptionBuilder.UseSqlServer(sqlConnectionStringBuilder.ConnectionString);
+        }
+
+        public DbSet<BlogModel> Blogs { get; set; }
+    }
+}
---
+++ DotNetTrainingBatch3.ConsoleApp/EFCore/EFCoreExamples.cs
@@ -0,0 +1,38 @@
+﻿using KoHein.ConsoleApp.EFCore.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoHein.ConsoleApp.EFCore
+{
+    public class EFCoreExamples
+    {
+        public void read()
+        {
+            AppDbContext db = new AppDbContext();
+            List<BlogModel> ls = db.Blogs.ToList();
+            foreach (BlogModel model in ls)
+            {
+                Console.WriteLine(model.BlogTitle);
+                Console.WriteLine(model.BlogAuthor);
+                Console.WriteLine(model.BlogContent);
+            }
+        }
+
+        public void Edit(int id)
+        {
+            AppDbContext db = new AppDbContext();
+            BlogModel item = db.Blogs.FirstOrDefault(item => item.BlogId == id);
+            if (item is null)
+            {
+                Console.WriteLine("No Data Found.");
+
+            }
+            Console.WriteLine(item.BlogTitle);
+            Console.WriteLine(item.BlogAuthor);
+            Console.WriteLine(item.BlogContent);
+        }
+    }
+}
