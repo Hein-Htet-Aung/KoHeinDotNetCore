05bc4ed2db960fe5a935a1561c97ec016d6a29dd
Hein Htet <ayathar123@gmail.com>
2024-04-07 12:24:51 +0630

    LoginUser

---
+++ KoHeinDotNetCore.LoginUser/AppDBContext.cs
@@ -0,0 +1,30 @@
+﻿using KoHeinDotNetCore.LoginUser.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoHeinDotNetCore.LoginUser
+{
+    public class AppDBContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder OptionBuilder)
+        {
+            SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+            {
+                DataSource = @".\SQL2019E",
+                InitialCatalog = "TestDB",
+                UserID = "sa",
+                Password = "p@ssw0rd",
+                TrustServerCertificate = true
+            };
+            OptionBuilder.UseSqlServer(sqlConnectionStringBuilder.ConnectionString);
+        }
+
+        public DbSet<UsersModel> Users { get; set; }
+        //public DbSet<LoginModel> Login { get; set; }
+    }
+}
---
+++ KoHeinDotNetCore.LoginUser/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using KoHeinDotNetCore.LoginUser.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace KoHeinDotNetCore.LoginUser.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        
+        public IActionResult login()
+        {
+            return View();
+        }
+
+
+        
+    }
+}
